7e979e8a85117e95e36a6ad83feea7d0b948ba7d
Quixun <xuanphan742@gmail.com>
2024-08-15 14:20:41 +0700

    update bai6

--- bai6.js
+++ bai6.js
@@ -1,5 +1,5 @@
-function filterEvenNumbers(arr) {
-  return (newArray = arr.filter((number) => number % 2 === 0));
+const filterEvenNumbers = (arr) => {
+  return arr.filter((number) => number % 2 === 0);
 }
 
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
