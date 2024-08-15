5a9fa17fc3beb01ead4805bc1aeff4b8a791a1b1
Quixun <xuanphan742@gmail.com>
2024-08-15 16:01:03 +0700

    fixed problem

--- bai8.js
+++ bai8.js
@@ -1,12 +1,11 @@
 const countOccurrences = (list, target) => {
-  const item = list.split("");
   var count = 0;
-  for (let i = 0; i < item.length; i++) {
-    if (item[i] === target) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === target) {
       count++;
     }
   }
   return `${target} appears ${count} times`;
 };
 
-console.log(countOccurrences("abkcejhdak", "j"));
+console.log(countOccurrences(['hello','xuanphan','quixuan','hey','hello'], "asdd"))
