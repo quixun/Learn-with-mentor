d9a1c980eafe41c7d28e6687e672751572954de8
Quixun <xuanphan742@gmail.com>
2024-08-15 14:28:20 +0700

    update bai2

--- bai2.js
+++ bai2.js
@@ -1,9 +1,9 @@
-function sumArray(arr) {
+const sumArray = (arr) => {
   var sum = 0;
   var arrLength = arr.length;
-  for (var i = 0; i < arrLength; i++) {
-    sum = sum + arr[i];
+  for (let i = 0; i < arrLength; i++) {
+    sum += arr[i];
   }
   return sum;
-}
+};
 console.log(sumArray([1, 2, 3, 4, 5]));
