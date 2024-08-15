772559aa75c2ea8a20047a26d110dffa40f4cb7b
Quixun <xuanphan742@gmail.com>
2024-08-15 14:14:18 +0700

    update bai5

--- bai5.js
+++ bai5.js
@@ -1,10 +1,11 @@
 function gradeCalculator(point) {
   if (point < 0) {
     return;
-  } else if (point <= 25) return "D";
-  else if (point > 25 && point <= 50) return "C";
-  else if (point > 50 && point <= 75) return "B";
-  else if (point > 75 && point <= 100) return "A";
+  }
+  if (point <= 25) return "D";
+  if (point > 25 && point <= 50) return "C";
+  if (point > 50 && point <= 75) return "B";
+  if (point > 75 && point <= 100) return "A";
 }
 
 console.log(gradeCalculator(Math.floor(Math.random() * 100)));
