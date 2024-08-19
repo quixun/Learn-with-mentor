2930a36aceaca6fa4987ee3cd956df67cc5e0487
Quixun <xuanphan742@gmail.com>
2024-08-19 10:24:31 +0700

    update bai10

--- bai10.js
+++ bai10.js
@@ -1,6 +1,5 @@
 const factorial1 = (x) => {
-  if (x === 0) return 1;
-  if (x === 1) return 1;
+  if (x === 0 || x === 1) return 1;
   return x * factorial1(x - 1);
 };
-console.log(factorial1(3));
+console.log(factorial1(0));
