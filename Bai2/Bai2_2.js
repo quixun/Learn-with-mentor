55b4d68a9be2fb62c3f76db64e2660574edd98e3
Quixun <xuanphan742@gmail.com>
2024-08-22 09:55:02 +0700

    Complete bai 2

---
+++ Bai2/Bai2_2.js
@@ -0,0 +1,12 @@
+/* 
+    2. Write a function that takes two arrays and returns a new array containing the elements that are present in both arrays.
+  - Example: 
+    + Input: arr1 = [1, 2, 3, 4], arr2 = [3, 4, 5, 6]
+    + Output: [3, 4]
+*/
+
+module.exports = filterArr = (arr1, arr2) => {
+  return arr1.filter((item) => arr2.includes(item));
+};
+
+
