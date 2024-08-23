d60e8cda16212b1200de79dee3b1df56c5fb899e
Quixun <xuanphan742@gmail.com>
2024-08-23 14:12:07 +0700

    Done updating more case for test

--- Bai2/bai2_2.test.js
+++ Bai2/bai2_2.test.js
@@ -2,3 +2,11 @@ const filterArr = require("./bai2_2");
 test("compare arr1 and arr2 return the elements that are present in both arrays", () => {
   expect(filterArr([1, 2, 3, 4], [3, 4, 5, 6])).toStrictEqual([3, 4]);
 });
+
+test("compare arr1 and arr2 return the elements that are present in both arrays", () => {
+  expect(filterArr([1, 2, 3, 4], [])).toStrictEqual([]);
+});
+
+test("compare arr1 and arr2 return the elements that are present in both arrays", () => {
+  expect(filterArr(["a", "b", "c"], ["a", "c"])).toStrictEqual(["a", "c"]);
+});
