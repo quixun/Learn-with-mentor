1f9ec71c8a4a0fab228eb4207eef2b70d7d6d720
Quixun <xuanphan742@gmail.com>
2024-08-23 14:49:55 +0700

    Update format and description

--- bai1/bai_1.test.js
+++ bai1/bai_1.test.js
@@ -4,6 +4,6 @@ test("isEven should return properly value", () => {
   expect(isEven(2)).toEqual(true);
 });
 
-test("isEven should return false value", () => {
+test("isEven should return false with old number", () => {
   expect(isEven(3)).toEqual(false);
 });
--- bai1/bai_10.test.js
+++ bai1/bai_10.test.js
@@ -1,11 +1,13 @@
 const factorial = require("./bai_10.js");
 
-test("factorial function should return 1 when it's received parameter which is 0 or 1", () => {
+test("factorial of 0 should return 1", () => {
   expect(factorial(0)).toEqual(1);
 });
-test("factorial function should return 1 when it's received parameter which 0 or 1", () => {
+
+test("factorial of 1 should return 1", () => {
   expect(factorial(1)).toEqual(1);
 });
-test("factorial function should return 6 when it's received parameter which is 3", () => {
+
+test("factorial of 3 should return 6", () => {
   expect(factorial(3)).toEqual(6);
 });
