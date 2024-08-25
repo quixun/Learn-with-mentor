289efe655c34b25945f9026b6f27e328e3cf42d8
Quixun <xuanphan742@gmail.com>
2024-08-25 21:11:39 +0700

    update test description for bai2_2

--- Bai2/bai2_2.test.js
+++ Bai2/bai2_2.test.js
@@ -1,9 +1,9 @@
 const filterArr = require("./bai2_2");
 
-test("filterArr should return a new array including the elements that are present in both arrays", () => {
+test("filterArr should return a new array if two arrays have common elements", () => {
   expect(filterArr([1, 2, 3, 4], [3, 4, 5, 6])).toStrictEqual([3, 4]);
 });
 
-test("filterArr should return the null array when there is not any element present in both arrays", () => {
-  expect(filterArr([1, 2, 3, 4], [])).toStrictEqual([]);
+test("filterArr should return the empty array if two arrays have no common element", () => {
+  expect(filterArr([1, 2, 3, 4], [5, 6])).toStrictEqual([]);
 });
